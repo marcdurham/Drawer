4ca63ce881b9a37b3f51166c4a60476ff195ecc5
Marc Durham <marcdurham@gmail.com>
2019-04-02 15:52:01 -0700

    Initial commit

---
+++ WinCad/DrawTester.Designer.cs
@@ -0,0 +1,112 @@
+﻿namespace WinCad
+{
+    partial class DrawTester
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.drawButton = new System.Windows.Forms.ToolStripButton();
+            this.mainPicture = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.mainStatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.drawButton});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(800, 25);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // drawButton
+            // 
+            this.drawButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.drawButton.Name = "drawButton";
+            this.drawButton.Size = new System.Drawing.Size(38, 22);
+            this.drawButton.Text = "Draw";
+            // 
+            // mainPicture
+            // 
+            this.mainPicture.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainPicture.Location = new System.Drawing.Point(0, 25);
+            this.mainPicture.Name = "mainPicture";
+            this.mainPicture.Size = new System.Drawing.Size(800, 425);
+            this.mainPicture.TabIndex = 1;
+            this.mainPicture.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mainStatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // mainStatus
+            // 
+            this.mainStatus.Name = "mainStatus";
+            this.mainStatus.Size = new System.Drawing.Size(39, 17);
+            this.mainStatus.Text = "Ready";
+            // 
+            // DrawTester
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.mainPicture);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "DrawTester";
+            this.Text = "Draw Tester";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainPicture)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton drawButton;
+        private System.Windows.Forms.PictureBox mainPicture;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel mainStatus;
+    }
+}
+
--- WinCad/DrawTester.cs
+++ WinCad/DrawTester.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace WinCad
 {
-    public partial class Form1 : Form
+    public partial class DrawTester : Form
     {
-        public Form1()
+        public DrawTester()
         {
             InitializeComponent();
         }

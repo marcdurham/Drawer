0401c5fc2babf2d353a3438ed79073cec1285990
Marc Durham <marcdurham@gmail.com>
2019-04-02 18:07:41 -0700

    Use session instead of individual properties

--- WinCad/DrawTester.cs
+++ WinCad/DrawTester.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 
@@ -8,11 +7,7 @@ namespace WinCad
 
     public partial class DrawTester : Form
     {
-        DrawModes drawMode = DrawModes.Ready;
-        Point firstCorner = Point.Empty;
-        Point secondCorner = Point.Empty;
-        Polyline currentPolyline = null;
-        Canvas canvas = new Canvas();
+        DrawingSession session = new DrawingSession();
 
         public DrawTester()
         {
@@ -21,19 +16,19 @@ namespace WinCad
 
         private void drawPolylineButton_Click(object sender, EventArgs e)
         {
-            drawMode = DrawModes.StartDrawing;
+            session.Mode = DrawModes.StartDrawing;
             mainStatus.Text = "Start drawing polyline:";
         }
 
         private void importPictureButton_Click(object sender, EventArgs e)
         {
-            drawMode = DrawModes.ImportingPictureFirstCorner;
+            session.Mode = DrawModes.ImportingPictureFirstCorner;
             mainStatus.Text = "Click first corner:";
         }
 
         private void drawRectangle_Click(object sender, EventArgs e)
         {
-            drawMode = DrawModes.DrawingRectangleFirstCorner;
+            session.Mode = DrawModes.DrawingRectangleFirstCorner;
             mainStatus.Text = "Click first corner:";
         }
 
@@ -41,13 +36,13 @@ namespace WinCad
         {
             Graphics g = e.Graphics;
 
-            foreach (var image in canvas.InsertedImages)
+            foreach (var image in session.Canvas.InsertedImages)
                 g.DrawImage(image.Image, image.Rectangle);
 
-            foreach (var r in canvas.Rectangles)
+            foreach (var r in session.Canvas.Rectangles)
                 g.DrawRectangle(Pens.Blue, r);
 
-            foreach (var p in canvas.Polylines)
+            foreach (var p in session.Canvas.Polylines)
                 if (p.Points.Count > 1)
                     for (int i = 1; i < p.Points.Count; i++)
                         g.DrawLine(Pens.Green, p.Points[i - 1], p.Points[i]);
@@ -75,70 +70,70 @@ namespace WinCad
         {
             if (button != MouseButtons.Left)
             {
-                drawMode = DrawModes.Ready;
+                session.Mode = DrawModes.Ready;
                 mainStatus.Text = "Ready";
             }
-            else if (drawMode == DrawModes.StartDrawing)
+            else if (session.Mode == DrawModes.StartDrawing)
             {
-                currentPolyline = new Polyline();
-                currentPolyline.Points.Add(point);
-                canvas.Polylines.Add(currentPolyline);
-                drawMode = DrawModes.DrawingPolyline;
+                session.CurrentPolyline = new Polyline();
+                session.CurrentPolyline.Points.Add(point);
+                session.Canvas.Polylines.Add(session.CurrentPolyline);
+                session.Mode = DrawModes.DrawingPolyline;
                 mainStatus.Text = "Click to add points to the polyline:";
             }
-            else if (drawMode == DrawModes.DrawingPolyline)
+            else if (session.Mode == DrawModes.DrawingPolyline)
             {
-                currentPolyline.Points.Add(point);
+                session.CurrentPolyline.Points.Add(point);
                 mainPicture.Invalidate();
 
             }
-            else if (drawMode == DrawModes.ImportingPictureFirstCorner)
+            else if (session.Mode == DrawModes.ImportingPictureFirstCorner)
             {
-                firstCorner = point;
-                drawMode = DrawModes.ImportingPictureSecondCorner;
+                session.FirstCorner = point;
+                session.Mode = DrawModes.ImportingPictureSecondCorner;
                 mainStatus.Text = "Click second corner:";
             }
-            else if (drawMode == DrawModes.ImportingPictureSecondCorner)
+            else if (session.Mode == DrawModes.ImportingPictureSecondCorner)
             {
-                secondCorner = point;
+                session.SecondCorner = point;
                 var image = new InsertedImage(
                     image: Bitmap.FromFile(@"C:\Store\Garage.TIF"),
                     rectangle: new Rectangle(
-                    firstCorner.X,
-                    firstCorner.Y,
-                    Math.Abs(firstCorner.X - secondCorner.X),
-                    Math.Abs(firstCorner.Y - secondCorner.Y)));
+                    session.FirstCorner.X,
+                    session.FirstCorner.Y,
+                    Math.Abs(session.FirstCorner.X - session.SecondCorner.X),
+                    Math.Abs(session.FirstCorner.Y - session.SecondCorner.Y)));
 
-                canvas.InsertedImages.Add(image);
+                session.Canvas.InsertedImages.Add(image);
 
                 mainPicture.Invalidate();
 
-                drawMode = DrawModes.Ready;
+                session.Mode = DrawModes.Ready;
                 mainStatus.Text = "Ready";
-                firstCorner = Point.Empty;
-                secondCorner = Point.Empty;
+                session.FirstCorner = Point.Empty;
+                session.SecondCorner = Point.Empty;
             }
-            else if (drawMode == DrawModes.DrawingRectangleFirstCorner)
+            else if (session.Mode == DrawModes.DrawingRectangleFirstCorner)
             {
-                firstCorner = point;
-                drawMode = DrawModes.DrawingRectangleSecondCorner;
+                session.FirstCorner = point;
+                session.Mode = DrawModes.DrawingRectangleSecondCorner;
                 mainStatus.Text = "Click second corner:";
             }
-            else if (drawMode == DrawModes.DrawingRectangleSecondCorner)
+            else if (session.Mode == DrawModes.DrawingRectangleSecondCorner)
             {
-                secondCorner = point;
-                canvas.Rectangles.Add(new Rectangle(
-                    firstCorner.X,
-                    firstCorner.Y,
-                    Math.Abs(firstCorner.X - secondCorner.X),
-                    Math.Abs(firstCorner.Y - secondCorner.Y)));
+                session.SecondCorner = point;
+                session.Canvas.Rectangles.Add(new Rectangle(
+                    session.FirstCorner.X,
+                    session.FirstCorner.Y,
+                    Math.Abs(session.FirstCorner.X - session.SecondCorner.X),
+                    Math.Abs(session.FirstCorner.Y - session.SecondCorner.Y)));
 
                 mainPicture.Invalidate();
 
-                drawMode = DrawModes.Ready;
+                session.Mode = DrawModes.Ready;
                 mainStatus.Text = "Ready";
-                firstCorner = Point.Empty;
-                secondCorner = Point.Empty;
+                session.FirstCorner = Point.Empty;
+                session.SecondCorner = Point.Empty;
             }
             else
             {

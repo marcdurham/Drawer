a234d04dc82365f6221f88d916a6182ca9bb640b
Marc Durham <marcdurham@gmail.com>
2019-04-18 11:40:07 -0700

    Extract method: AllPointsFrom

--- WinCad/Zoomer.cs
+++ WinCad/Zoomer.cs
@@ -11,6 +11,38 @@ namespace WinCad
 
         public double ZoomFactorForExtents(SysSize size, Canvas canvas)
         {
+            List<Point> allPoints = AllPointsFrom(canvas);
+
+            if (allPoints.Count == 0)
+            {
+                return 0;
+            }
+
+            double maxX = allPoints.Max(p => p.X);
+            double minX = allPoints.Min(p => p.X);
+            double maxY = allPoints.Max(p => p.Y);
+            double minY = allPoints.Min(p => p.Y);
+
+            double xDelta = maxX - minX;
+            double yDelta = maxY - minY;
+
+            var paddedSize = new SysSize(
+                width: size.Width - Padding,
+                height: size.Height - Padding);
+
+            double xRatio = paddedSize.Width == 0
+                ? 0
+                : paddedSize.Width / xDelta;
+
+            double yRatio = paddedSize.Height == 0
+                ? 0
+                : paddedSize.Height / yDelta;
+
+            return Math.Min(xRatio, yRatio);
+        }
+
+        static List<Point> AllPointsFrom(Canvas canvas)
+        {
             var allPoints = new List<Point>();
 
             foreach (var l in canvas.Layers)
@@ -21,32 +53,7 @@ namespace WinCad
                 }
             }
 
-            if (allPoints.Count == 0)
-            {
-                return 0;
-            }
-
-            var paddedSize = new SysSize(
-              width: size.Width - Padding,
-              height: size.Height - Padding);
-
-            double maxX = allPoints.Max(p => p.X);
-            double minX = allPoints.Min(p => p.X);
-            double maxY = allPoints.Max(p => p.Y);
-            double minY = allPoints.Min(p => p.Y);
-
-            double xDelta = maxX - minX;
-            double yDelta = maxY - minY;
-
-            double xRatio = paddedSize.Width == 0 
-                ? 0 
-                : paddedSize.Width / xDelta;
-
-            double yRatio = paddedSize.Height == 0 
-                ? 0 
-                : paddedSize.Height / yDelta;
-
-            return Math.Min(xRatio, yRatio);
+            return allPoints;
         }
     }
 }

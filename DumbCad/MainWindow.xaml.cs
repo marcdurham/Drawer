471f6f9b558a979759acaf5a48fd6744329420b1
Marc Durham <marcdurham@gmail.com>
2021-01-16 16:09:45 -0800

    Detect poly line vertices

--- DumbCad/MainWindow.xaml.cs
+++ DumbCad/MainWindow.xaml.cs
@@ -15,6 +15,14 @@ namespace DumbCad
         DrawMode previousMode = DrawMode.Ready;
         float zoomFactor = 1f;
         List<SKPath> paths = new List<SKPath>();
+        List<Circle> Circles = new List<Circle>();
+        Circle circleStarting = new Circle();
+        SKPoint circleFinishingPoint = new SKPoint();
+        SKPoint polylineStarting = new SKPoint();
+        SKPath polylineFinishing = new SKPath();
+        SKPath polylineNextSegment = new SKPath();
+        SKPoint panStart = new SKPoint();
+        SKPoint panOffset = new SKPoint();
 
         SKPaint paintCircleFinished = new SKPaint()
         {
@@ -29,17 +37,6 @@ namespace DumbCad
             StrokeWidth = 2f,
             Color = SKColors.Blue
         };
-
-        List<Circle> Circles = new List<Circle>();
-        Circle circleStarting = new Circle();
-        SKPoint circleFinishingPoint = new SKPoint();
-        SKPoint polylineStarting = new SKPoint();
-        SKPath polylineFinishing = new SKPath();
-        SKPath polylineNextSegment = new SKPath();
-        int xCenter = 0;
-        int yCenter = 0;
-        SKPoint panStart = new SKPoint();
-        SKPoint panOffset = new SKPoint();
 
         public MainWindow()
         {
@@ -60,8 +57,6 @@ namespace DumbCad
 
             canvas.Clear(SKColors.Beige);
             canvas.Scale(zoomFactor, -zoomFactor);
-            xCenter = e.Info.Width/2;
-            yCenter = e.Info.Height/2;
 
             panOffsetLabel.Content = $"PanOffset: {panOffset.X:F3}, {panOffset.Y:F3}";
 
@@ -132,7 +127,23 @@ namespace DumbCad
                 {
                     if(path.Bounds.Contains(worldPoint.X, worldPoint.Y))
                     {
-                        System.Diagnostics.Debug.WriteLine("Got one");
+                        System.Diagnostics.Debug.WriteLine($"Path {paths.IndexOf(path)} Checking...");
+                        //Math.Sin()
+                        //System.Diagnostics.Debug.WriteLine($"  Vertex 0: {path.Points[0].X:F3}, {path.Points[0].Y:F3}");
+                        
+                        for (int i = 0; i < path.Points.Length; i += 2)
+                        {
+                            //var a = path.Points[i - 1];
+                            var b = path.Points[i];
+                            var m = worldPoint;
+                            var dist = Geometry.Distance(m, b);
+                            //System.Diagnostics.Debug.WriteLine($"  Vertex {i-1}: {path.Points[i-1].X:F3}, {path.Points[i-1].Y:F3}");
+                            System.Diagnostics.Debug.WriteLine($"  Vertex {i}: {path.Points[i].X:F3}, {path.Points[i].Y:F3} Distance: {dist:F3} {dist <= 5f}");
+                            //if(dist <= 5.0f)
+                            //{
+                            //    System.Diagnostics.Debug.WriteLine("  Got one");
+                            //}
+                        }
                     }
                 }
             }
